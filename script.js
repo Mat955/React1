87dccbba0e738d38cef2cf689e2ac53fe97e743c
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-05-24 20:30:59 +0200

    change structures in function script.js

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-var movies = [{
-        id: 1,
-        title: 'Harry Potter',
-        desc: 'film o czarodzieju',
-        photo: 'https://www.bookhunter.pl/okladki/352/large/book_352_large_1.jpg'
-    },
-    {
-        id: 2,
-        title: 'Krol Lew',
-        desc: 'Film o krolu sawanny',
-    }
-];
-
-var moviesElements = movies.map(function (movies) {
-    return React.createElement('li', {
-            key: movies.id
-        },
-        React.createElement('h2', {}, movies.title),
-        React.createElement('p', {}, movies.desc),
-        React.createElement('img', {
-            src: movies.photo
-        })
-    );
-});
-
-var elementFilms =
-    React.createElement('div', {},
-        React.createElement('h1', {}, 'List of Films'),
-        React.createElement('ul', {}, moviesElements)
-    );
-
-
-var theBestGames = [{
+var elementsOfList = [{
         id: 1,
         title: 'Counter Strike',
         desc: 'Game ‎first-person shooter',
@@ -40,26 +8,36 @@ var theBestGames = [{
         id: 2,
         title: 'Red redemption 2',
         desc: 'Western action-adventure game'
+    },
+    {
+        id: 3,
+        title: 'Harry Potter',
+        desc: 'film o czarodzieju',
+        photo: 'https://www.bookhunter.pl/okladki/352/large/book_352_large_1.jpg'
+    },
+    {
+        id: 4,
+        title: 'Krol Lew',
+        desc: 'Film o krolu sawanny',
     }
 ];
 
-var gamesElements = theBestGames.map(function (theBestGames) {
+var allElements = elementsOfList.map(function (elementsOfList) {
     return React.createElement('li', {
-            key: theBestGames.id
+            key: elementsOfList.id
         },
-        React.createElement('h2', {}, theBestGames.title),
-        React.createElement('p', {}, theBestGames.desc),
+        React.createElement('h2', {}, elementsOfList.title),
+        React.createElement('p', {}, elementsOfList.desc),
         React.createElement('img', {
-            src: theBestGames.photo
+            src: elementsOfList.photo
         })
     );
 });
 
-var elementGames =
+var elementsOfList =
     React.createElement('div', {},
-        React.createElement('h1', {}, 'List of The Best Games'),
-        React.createElement('ul', {}, gamesElements)
+        React.createElement('h1', {}, 'List of The Best Games & Films'),
+        React.createElement('ul', {}, allElements)
     );
 
-ReactDOM.render(elementGames, document.getElementById('app-games'));
-ReactDOM.render(elementFilms, document.getElementById('app-films'));
+ReactDOM.render(elementsOfList, document.getElementById('app-all'));

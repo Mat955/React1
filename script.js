91f412f8a1fbdc1ef8cd39773dc766dd02ecee35
Mateusz Lewartowski <lewar2013c@gmail.com>
2019-06-16 20:20:31 +0200

    finish task

--- script.js
+++ script.js
@@ -1,43 +1,69 @@
-var elementsOfList = [{
-        id: 1,
-        title: 'Counter Strike',
-        desc: 'Game ‎first-person shooter',
-        photo: 'https://cdn.shopify.com/s/files/1/0630/8509/products/pst0297reddead_large.jpg?v=1533063544'
-    },
-    {
-        id: 2,
-        title: 'Red redemption 2',
-        desc: 'Western action-adventure game'
-    },
-    {
-        id: 3,
-        title: 'Harry Potter',
-        desc: 'film o czarodzieju',
-        photo: 'https://www.bookhunter.pl/okladki/352/large/book_352_large_1.jpg'
-    },
-    {
-        id: 4,
-        title: 'Krol Lew',
-        desc: 'Film o krolu sawanny',
-    }
+var movies = [{
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju',
+    photo: 'https://ssl-gfx.filmweb.pl/po/57/46/95746/7537945.3.jpg'
+},
+{
+    id: 2,
+    title: 'Krol Lew',
+    desc: 'Film o krolu sawanny',
+    photo: 'https://www.bookhunter.pl/okladki/352/large/book_352_large_1.jpg'
+}
 ];
 
-var allElements = elementsOfList.map(function (elementsOfList) {
-    return React.createElement('li', {
-            key: elementsOfList.id
+var theBestGames = [{
+    id: 1,
+    title: 'Counter Strike',
+    desc: 'Game ‎first-person shooter',
+    photo: 'https://0.allegroimg.com/s1440/034e59/456e62184b0daa6366dee3e87e60'
+},
+{
+    id: 2,
+    title: 'Red redemption 2',
+    desc: 'Western action-adventure game',
+    photo: 'https://cdn.shopify.com/s/files/1/0630/8509/products/pst0297reddead_large.jpg?v=1533063544'
+}
+];
+
+myFunction = (theBestGames, movies) => {
+    if (theBestGames) {
+        return React.createElement('li', {
+            key: theBestGames.id
         },
-        React.createElement('h2', {}, elementsOfList.title),
-        React.createElement('p', {}, elementsOfList.desc),
-        React.createElement('img', {
-            src: elementsOfList.photo
-        })
-    );
-});
+            React.createElement('h2', {}, theBestGames.title),
+            React.createElement('p', {}, theBestGames.desc),
+            React.createElement('img', {
+                src: theBestGames.photo
+            }),
+        );
+    } if (movies) {
+        return React.createElement('li', {
+            key: movies.id
+        },
+            React.createElement('h2', {}, movies.title),
+            React.createElement('p', {}, movies.desc),
+            React.createElement('img', {
+                src: movies.photo
+            }),
+        );
+    }
+}
 
-var elementsOfList =
+var moviesElements = movies.map(myFunction);
+var allGamesElements = theBestGames.map(myFunction);
+
+var elementFilms =
     React.createElement('div', {},
-        React.createElement('h1', {}, 'List of The Best Games & Films'),
-        React.createElement('ul', {}, allElements)
+        React.createElement('h1', {}, 'List of Films'),
+        React.createElement('ul', {}, moviesElements)
     );
 
-ReactDOM.render(elementsOfList, document.getElementById('app-all'));
+var elementGames =
+    React.createElement('div', {},
+        React.createElement('h1', {}, 'List of The Best Games'),
+        React.createElement('ul', {}, allGamesElements)
+    );
+
+ReactDOM.render(elementGames, document.getElementById('app-games'));
+ReactDOM.render(elementFilms, document.getElementById('app-films')); 
